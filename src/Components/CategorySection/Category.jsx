6ecbbf5a8db7a86f96581f692c279e149d416ad4
arjunbector <arjunbector@gmail.com>
2023-11-05 21:18:36 +0530

    Added functionality to view more button in category page

--- src/Components/CategorySection/Category.jsx
+++ src/Components/CategorySection/Category.jsx
@@ -9,9 +9,13 @@ const Category = (props) => {
 const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState(null);
   const [newsCards, setNewsCards] = useState(null);
-  const getDataBusiness = () => {
-    console.log(props.category.toLowerCase());
-    fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${props.category.toLowerCase()}&pageSize=4&apiKey=${api_key}`)
+  const [buttonState, setButtonState] = useState("View More")
+  const handleClick = ()=>{
+    setButtonState("");
+    getData(20);
+  }
+  const getData = (numberOfCards) => {
+    fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${props.category.toLowerCase()}&pageSize=${numberOfCards}&apiKey=${api_key}`)
       .then((response) => response.json())
       .then((data) => {
         setIsLoading(false);
@@ -34,21 +38,18 @@ const [isLoading, setIsLoading] = useState(true);
   };
 
   const skeletonCardsArray = [
-    <SkeletonCard />,
-    <SkeletonCard />,
-    <SkeletonCard />,
-    <SkeletonCard />,
+    Array(4).fill(<SkeletonCard/>)
   ];
 
   useEffect(() => {
-    getDataBusiness();
+    getData(4);
   }, []);
   return (
     <div>
       <div className={styles.category_section}>
         <h1 className={styles.category_heading}>{props.category}</h1>
         <div className={styles.cards_section}>{isLoading? skeletonCardsArray: newsCards}</div>
-        <button className={styles.view_more_btn}>View More</button>
+        {buttonState &&  <button className={styles.view_more_btn} onClick={handleClick}>{buttonState}</button>}
       </div>
     </div>
   );
